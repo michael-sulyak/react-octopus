af554f202badb1032c01735121b6ee56018c2942
Michael Sulyak <m.sulyak@gmail.com>
2018-12-16 21:17:50 +0700

    Updates

---
+++ precache-manifest.d95fd557e28b8dcd3aaf6e9383bac05a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8d45ee6af1d45776f19",
+    "url": "/react-octopus/static/js/runtime~main.b8d45ee6.js"
+  },
+  {
+    "revision": "07624fb2a637fc8b187d",
+    "url": "/react-octopus/static/js/main.07624fb2.chunk.js"
+  },
+  {
+    "revision": "8697a82572b2584e59bc",
+    "url": "/react-octopus/static/js/1.8697a825.chunk.js"
+  },
+  {
+    "revision": "07624fb2a637fc8b187d",
+    "url": "/react-octopus/static/css/main.560de242.chunk.css"
+  },
+  {
+    "revision": "2a96396fdef9c73eaf6d798823a8cbaa",
+    "url": "/react-octopus/index.html"
+  }
+];
